223141ace09f277222961bf1e4cd4c135dd03c2d
Ben Elkin <8xe8n-cm@online.de>
2023-07-22 22:35:57 +0200

    get statistics

--- src/VisualCrossing.jl
+++ src/VisualCrossing.jl
@@ -1,6 +1,6 @@
 module VisualCrossing
 
-using Scratch, Dates, HTTP, DataFrames, JSON3, Glob
+using Scratch, Dates, HTTP, DataFrames, JSON3, Glob, Missings
 
 include("file_functions.jl")
 
@@ -63,7 +63,7 @@ end
 function dayfromapril1st(d)
     y = year(d)
     april1st = dayofyear(Date(y, 4, 1))
-    return dayofyear(d) - april1st + 1
+    return dayofyear(d) - april1st # zero-indexing :)
 end
 
 export dayfromapril1st
@@ -79,9 +79,10 @@ function json2df(source, fromfile=true)
     # jsondata = String(jsondata)
     data = JSON3.read(jsondata).days
     df = DataFrame(data)
+    miss4nothing!(df)
     transform!(df, :datetime => ByRow(Date) => :Date)
-    transform!(df, :Date => ByRow(dayfromapril1st) => :DayFromApril1st)
-    select!(df, :Date, :DayFromApril1st, Not(:datetime))
+    transform!(df, :Date => ByRow(dayfromapril1st) => :DayFromApril1st, :Date => ByRow(year) => :Year)
+    select!(df, :Date, :Year, :DayFromApril1st, Not(:datetime))
     return df
 end
 
@@ -100,8 +101,37 @@ function fetch_years(lastyear, years=8)
     end
 end
 
+function span(d1::Date, d2::Date)
+    day1 = dayfromapril1st(d1)
+    day2 = dayfromapril1st(d2)
+    return (; day1, day2)
+end
 
+span(d1::AbstractString, d2::AbstractString) = span(Date(d1), Date(d2))
 
-export wfetch, period_data, fetch_period, json2df, fetch_years, data_file_path, json2df
+span(d1::Nothing=nothing, d2::Nothing=nothing) = span("2015-05-15", "2015-09-15")
+
+span(d1::Integer, d2::Integer) = (; day1=d1, day2=d2)
+
+function filterspan(df, d1=nothing, d2=nothing)
+    (; day1, day2) = span(d1, d2)
+    return subset(df, :DayFromApril1st=> x -> day1 .<= x .<= day2)
+end
+
+function groupstat(df, fn, sourcecol=nothing, d1=nothing, d2=nothing)
+    df = deepcopy(df)
+    gdf = groupby(filterspan(df, d1, d2), :Year)
+    cumstat = combine(gdf, sourcecol => fn => :cumstat)
+    return cumstat
+end
+
+function miss4nothing!(df)
+    for nm in names(df)
+        df[!, nm] = replace(df[!, nm], nothing => missing)
+    end
+    return df
+end
+
+export wfetch, period_data, fetch_period, json2df, fetch_years, data_file_path, json2df, span, filterspan, groupstat, miss4nothing!
 
 end # module VisualCrossing
